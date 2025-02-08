1f59276dc374dbb0d1ae166ac099dfda76ca98c2
sivertheisholt <sivert.b.heisholt@protonmail.com>
2025-02-08 20:56:11 +0100

    Add webfinger

---
+++ SivertHeisholt.Api/Controllers/WebFingerController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SivertHeisholt.Api.Controllers;
+
+[ApiController]
+[Route("/.well-known/webfinger")]
+public class WebFingerController : ControllerBase
+{
+  [HttpGet]
+  public IActionResult Get([FromQuery] string resource)
+  {
+    var response = new
+    {
+      subject = resource,
+      links = new[]
+        {
+                new
+                {
+                    rel = "http://openid.net/specs/connect/1.0/issuer",
+                    href = "https://sivertheisholt.no"
+                }
+            }
+    };
+
+    return Ok(response);
+  }
+}
